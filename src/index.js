7d74471a76ddae51e402f42a0a8c00abcc72f6ff
bendn <bend.n@outlook.com>
2022-05-05 07:14:33 +0700

    yay

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ const PORT = process.env.PORT || 3000;
 const wss = new WebSocket.Server({ port: PORT })
 console.log(`Server started on port ${PORT}`)
 wss.on('connection', ws => {
+  console.log(wss.clients)
   console.log('connected')
   ws.on('message', (message) => {
     let recieveBuff = Buffer.from(message)
     let recieve = gdCom.getVar(recieveBuff)
     console.log(recieve.value)
-    // let buffer = gdCom.putVar(Math.random())
     // ws.send(buffer)
   })
 })
